04f52cb4c28a1db95c12149d6dd5905192ecaa33
lorikmusaj5759 <147962295+lorikmusaj5759@users.noreply.github.com>
2025-07-07 08:39:46 +0300

    Create file-input.ts

---
+++ ui/components/app/snaps/snap-ui-renderer/components/file-input.ts
@@ -0,0 +1,14 @@
+import { FileInputElement } from '@lorikapp/snaps-sdk/jsx';
+
+import { UIComponentFactory } from './types';
+
+export const fileInput: UIComponentFactory<FileInputElement> = ({ element, form }) => ({
+  element: 'SnapUIFileInput',
+  props: {
+    name: element.props.name,
+    accept: element.props.accept,
+    compact: element.props.compact,
+    disabled: element.props.disabled,
+    form,
+  },
+});
